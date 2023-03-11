9d6ddfb9ed7fc52a1e40df60951fb94ba3399aa3
damian97 <sryncu@o2.pl>
2023-03-11 19:02:27 +0100

    Added some text in Main

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,14 @@ public class Main {
         System.out.println("Bez zewnetrznych bibliotek");
         System.out.println();
         System.out.println("Trzeba zrobić Jara i GUI, chociaz to moze dzialac z konsoli");
+        System.out.println();
+        System.out.println("Dodanie troche znakow");
+
+            // Ciekawy zbior zadanek z Programowania
+        //
+        //   http://aragorn.pb.bialystok.pl/~ania/Dzienne/podst_prog/zbior_zadan_PP.pdf
+        //
+
 
     }
 }
