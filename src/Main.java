b8def4f1465aef953a132e582120631245285bea
damian97 <sryncu@o2.pl>
2023-03-14 19:08:25 +0100

    Added Tibia Class

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,12 @@ public class Main {
 
 
         String urlInteriaReg = "https://konto-pocztowe.interia.pl/#/nowe-konto/darmowe";
+        String urlTibiaReg = "https://www.tibia.com/account/?subtopic=createaccount";
         int delaySpeed = 10;
 
         Color idleButton = new Color(0, 85, 255);
         Color highlightedButton = new Color(0, 69, 207);
+        Color black = new Color(0, 0, 0);
 
 
         RobotManage rb = new RobotManage();
@@ -18,9 +20,8 @@ public class Main {
         User user = new User();
         user.printUser();
 
-
-//        Interia interia = new Interia(urlInteriaReg);
-//        rb.eneterRegDetail();
+        Interia interia = new Interia(urlInteriaReg);
+        rb.eneterRegDetail();
 //        rb.enterCharacters(user.getfName());
 //        rb.pressTab();
 //        rb.enterCharacters(user.getsName());
@@ -33,16 +34,30 @@ public class Main {
 //        rb.pressTab();
 //        rb.enterOptionPane(user.getGender());
 //        rb.pressTab();
+//        rb.cleanTextArea();
 //        rb.enterCharacters(user.getNickName());
 //        rb.pressTab(2);
 //        rb.enterCharacters(user.getPass());
 //        rb.pressTab();
 //        rb.enterCharacters(user.getPass());
+//
+//        rb.searchColors(black);
+//        rb.searchColors(idleButton, highlightedButton);
+//
+//        rb.closeCard();
 
-        rb.searchColor(idleButton);
 
-
-
+        Interia tibia = new Interia(urlTibiaReg);
+        rb.sleep(3000);
+        rb.enterCharacters(user.getNickName());
+        rb.enterAt();
+        rb.enterCharacters("interia.pl");
+        rb.pressTab();
+        rb.enterCharacters(user.getPass());
+        rb.pressTab();
+        rb.enterCharacters(user.getPass());
+        rb.pressTab(2);
+        rb.pressEnter();
 
     }
 }
--- src/RobotManage.java
+++ src/RobotManage.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 
 public class RobotManage {
 
@@ -42,6 +43,13 @@ public class RobotManage {
 
    }
 
+   public void sleep(int ms) {
+      try {
+         Thread.sleep(ms);
+      } catch (InterruptedException e) {
+         throw new RuntimeException(e);
+      }
+   }
 
    public void enterCharacters(String string) {
       for (int i = 0; i < string.length(); i++) {
@@ -59,7 +67,6 @@ public class RobotManage {
 
    }
 
-
    public void enterOptionPane(int value) {
       for (int i = 0; i < value; i++) {
          robot.keyPress(KeyEvent.VK_DOWN);
@@ -71,6 +78,21 @@ public class RobotManage {
       robot.delay(500);
    }
 
+   public void enterAt() {
+      robot.keyPress(KeyEvent.VK_SHIFT);
+      robot.delay(50);
+      robot.keyPress(KeyEvent.VK_2);
+      robot.delay(50);
+      robot.keyRelease(KeyEvent.VK_SHIFT);
+      robot.delay(50);
+      robot.keyRelease(KeyEvent.VK_2);
+      robot.delay(150);
+   }
+
+   public void pressEnter() {
+      robot.keyPress(KeyEvent.VK_ENTER);
+      robot.keyRelease(KeyEvent.VK_ENTER);
+   }
    public void pressTab() {
       robot.keyPress(KeyEvent.VK_TAB);
       robot.keyRelease(KeyEvent.VK_TAB);
@@ -84,31 +106,86 @@ public class RobotManage {
    }
 
 
-   public void searchColor(Color color) {
+   public void cleanTextArea() {
+//      robot.keyPress(KeyEvent.VK_CONTROL);
+//      robot.delay(50);
+//      robot.keyPress(KeyEvent.VK_A);
+//      robot.delay(50);
+//      robot.keyRelease(KeyEvent.VK_CONTROL);
+//      robot.delay(50);
+//      robot.keyRelease(KeyEvent.VK_A);
+//      robot.delay(50);
+      robot.keyPress(KeyEvent.VK_BACK_SPACE);
+      robot.delay(3050);
+      robot.keyRelease(KeyEvent.VK_BACK_SPACE);
+      robot.delay(500);
+   }
+
+
+   public void closeCard() {
+      robot.delay(5000);
+      robot.keyPress(KeyEvent.VK_CONTROL);
+      robot.delay(50);
+      robot.keyPress(KeyEvent.VK_W);
+      robot.delay(50);
+      robot.keyRelease(KeyEvent.VK_CONTROL);
+      robot.delay(50);
+      robot.keyRelease(KeyEvent.VK_W);
+      robot.delay(150);
+   }
+
+
+   public void searchColors(Color color, Color color2) {
       Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-      int screenWidth = screenSize.width + 1000;
+      int screenWidth = screenSize.width;
       int screenHeight = screenSize.height;
 
       robot.setAutoDelay(0);
 
 
-      int x = 0, y = 0;
-      while (x < screenWidth) {
-         y += 20;
+      int x = screenWidth/2, y = screenHeight/4;
 
-         if (robot.getPixelColor(x, y).equals(color)) {
+      for (; y < screenHeight; y += 5) {
+         Color tmpColor = robot.getPixelColor(x, y);
+         if (tmpColor.equals(color)) {
             robot.mouseMove(x, y);
-            robot.setAutoDelay(getDelaySpeed());
-            break;
+            robot.delay(500);
+            tmpColor = robot.getPixelColor(x, y);
+            if (tmpColor.equals(color2)) {
+               robot.mousePress(MouseEvent.BUTTON1_DOWN_MASK);
+               robot.mouseRelease(MouseEvent.BUTTON1_DOWN_MASK);
+               break;
+            } else {
+               System.out.println("Nie znaleziono");
+            }
          }
-
-         if (y >= screenHeight) {
-            y = 0;
-            x += 25;
-         }
-
-         robot.mouseMove(x, y);
       }
+
+      robot.setAutoDelay(10);
+
+
+   }
+
+   public void searchColors(Color black) {
+      Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+      int screenWidth = screenSize.width;
+      int screenHeight = screenSize.height;
+
+      robot.setAutoDelay(0);
+
+
+      int x = screenWidth/2, y = screenHeight/4;
+
+      for (; y < screenHeight; y += 5) {
+         Color tmpColor = robot.getPixelColor(x, y);
+         if (tmpColor.equals(black)) {
+            robot.mouseMove(x, y);
+            robot.delay(500);
+            tmpColor = robot.getPixelColor(x, y);
+         }
+      }
+
+      robot.setAutoDelay(10);
 
    }
 
